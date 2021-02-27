8b3a0ac2cf496737aa7fcfbeca7a9a5ef4b5de73
nemo-xhan <fe-hex@protonmail.com>
2021-02-27 09:16:55 +0500

    add some exceptions

--- main.py
+++ main.py
@@ -13,8 +13,14 @@ else:
     url = input("\tYOUTUBE VIDEO URL: ")
 
 print(f'\tURL : {url}')
-yt = pt.YouTube(url)
-streams = yt.streams
+
+try:
+    yt = pt.YouTube(url)
+    streams = yt.streams
+except pt.exceptions.RegexMatchError:
+    print("\t\tINCORRECT URL");
+    exit(1)
+
 print(f"\n\n\tTITLE: {yt.title}")
 print(f"\tAUTHOR: {yt.author}")
 print(f"\tVIEWS: {yt.views}")
@@ -25,20 +31,24 @@ for i, stream in enumerate(streams):
 get_choice = True
 
 while get_choice:
-    choice = int(input(f"\tSelect [ 0 - {len(streams)-1}]: "))
-    if choice < len(streams):
-        print("\tDownloading Video...")
+    try: 
+        choice = int(input(f"\tSelect [ 0 - {len(streams)-1}]: "))
+        if choice < len(streams):
+            print("\tDownloading Video...")
     
-        try:
-            with requests.get(streams[choice].url, stream=True) as r:
-                with open(streams[choice].default_filename, "wb") as f:
-                    for chunk in r.iter_content(512):
-                        f.write(chunk)
-            print("\tDownloading Completed")
-            get_choice = False
-        except Exception as e:
-            print(str(e))
-            exit(1)
-    else:
-        print("\tInvalid Choice")
+            try:
+                with requests.get(streams[choice].url, stream=True) as r:
+                    with open(streams[choice].default_filename, "wb") as f:
+                        for chunk in r.iter_content(512):
+                            f.write(chunk)
+                print("\tDownloading Completed")
+                get_choice = False
+            except Exception as e:
+                print(str(e))
+                exit(1)
+        else:
+            print("\tInvalid Choice")
+    except Exception as e:
+        print(str(e))
+        
 
